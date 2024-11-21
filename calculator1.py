e07f36e16a46ef50c48e8127f277497333b5df6d
codewithalex07 <imailprajwalsb@gmail.com>
2024-11-21 22:43:08 +0530

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enetr a second number: "))
+
+add = number1 + number2
+print("The sum of ", number1, "and", number2, "is", add)
+sub = number1 - number2
+print("the subtraction of", number1, "and", number2, "is", sub)
+multiplication = number1 * number2
+print("the multiplication of", number1, "and", number2, "is", multiplication)
+div = number1 / number2
+print("the divison of", number1, "and", number2, "is", div)
